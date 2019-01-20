1f521519c709891bb8f6557925937e5ec9e20527
jskny <jskny.goo@gmail.com>
2019-01-20 13:05:08 +0900

    change dir.

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿/*
+ * Flowers-ink
+ * 2019/1/20
+ * jskny
+*/
+
+// expressモジュールを読み込む
+const express = require('express');
+
+// expressアプリを生成する
+const app = express();
+
+
+// 静的ファイルの場所指定
+app.use(express.static('static-files'));
+
+
+// サーバーの起動
+app.listen(process.env.PORT || 3000);
+console.log("PORT : 3000");
+
