abc704c2eb946e3eaa04708a92eea4d910267dc0
jskny <jskny.goo@gmail.com>
2019-01-23 21:35:10 +0900

    add signin-add-user

---
+++ routes/signin-add-user.js
@@ -0,0 +1,11 @@
+﻿var express = require('express');
+var router = express.Router();
+
+router.post('/', function(req, res) {
+console.log("send signinAddUser : " + req.body.uid + "<>" + req.body.upass);
+	res.render('signin');
+});
+
+
+module.exports = router;
+
