807e19557af5349295d30d9acba268e25f507446
jskny <jskny.goo@gmail.com>
2019-01-27 12:06:59 +0900

    add login sample and bug fix for common-header.

--- routes/login.js
+++ routes/login.js
@@ -7,5 +7,18 @@ console.log("send login.ejs");
 });
 
 
+// ログイン処理
+router.post('/', function(req, res) {
+console.log("login checker");
+console.log(req.body.uid + "<>" + req.body.upass);
+	if (req.body.uid == "test" && req.body.upass == "test123") {
+		res.render('mypage');
+	}
+	else {
+		res.render('login', { msg: "user id or pass isn't correct." });
+	}
+});
+
+
 module.exports = router;
 
